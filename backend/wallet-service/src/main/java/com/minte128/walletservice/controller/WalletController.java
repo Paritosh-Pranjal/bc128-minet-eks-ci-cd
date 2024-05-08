1dc78b6e264e20c0193b0f487ec6a5cb8fc3b3b9
paritosh-pranjal <paritosh.pranjal@zemosolabs.com>
2024-05-08 23:20:25 +0530

    fix: fix path

--- backend/wallet-service/src/main/java/com/minte128/walletservice/controller/WalletController.java
+++ backend/wallet-service/src/main/java/com/minte128/walletservice/controller/WalletController.java
@@ -10,7 +10,6 @@ import org.springframework.validation.annotation.Validated;
 
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/api/v1/wallets")
 @Validated
